4ce6389565d8e583fe0368ee559b7b2d4ffd8224
Marco Behler <marco.behler@jetbrains.com>
2021-01-28 16:01:32 +0100

    initial commit

---
+++ src/main/java/com/jetbrains/mavendemo/MavenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jetbrains.mavendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jetbrains/mavendemo/MavenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.mavendemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
